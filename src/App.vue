11303f68cc75ad8e7a5ff5e291b5c10cfe3645d1
andreasvirkus <andreasvirkus@gmail.com>
2021-11-27 14:55:47 +0200

    Add intro trigger

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <img alt="Klaus" class="logo" src="./assets/logo.svg" @click="reload" />
+
+  <button class="info-btn" @click="showIntro">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-info"
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <line x1="12" y1="16" x2="12" y2="12"></line>
+      <line x1="12" y1="8" x2="12.01" y2="8"></line>
+    </svg>
+  </button>
+
   <p>Score: {{ score }}</p>
   <p>Guesses left: {{ GAME_LENGTH - guessCount + 1 }}</p>
 
@@ -121,5 +141,24 @@ button {
 button:hover {
   background-color: var(--brand-50);
   color: white;
+}
+
+.info-btn {
+  position: absolute;
+  top: 32px;
+  left: 32px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  padding: 0;
+}
+.info-btn svg {
+  height: 16px;
+  width: 16px;
 }
 </style>
